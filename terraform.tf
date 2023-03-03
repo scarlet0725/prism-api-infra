67096f56ff60544c997b5276b7ad180831333c00
scarlet0725 <20381860+scarlet0725@users.noreply.github.com>
2023-03-04 02:14:03 +0900

    feat: backendをS3に移行

--- terraform.tf
+++ terraform.tf
@@ -2,3 +2,17 @@ provider "google" {
   project = "prism-prod-372103"
   region  = "asia-northeast1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "irofessional-main-tfstate"
+    key    = "prism-api-infra/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.55.0"
+    }
+  }
+}
